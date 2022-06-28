287143b51f8c35c8a8d51c915c4886bdc21011bc
Saied Seghatoleslami <seghatoleslami74@gmail.com>
2022-06-28 10:09:27 -0400

    first fully functioning submit with some link resilliancy

--- EME_Head_End.ino
+++ EME_Head_End.ino
@@ -14,16 +14,19 @@ Its functions are:
 
 //Static IP address will be used to keep the code size small
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; //TODO MAC address from the Arduino ETH shield sticker
-IPAddress ip(192, 168, 0, 78); //somewhat arbitrary private IP accress
+IPAddress ip(192, 168, 0, 20); //somewhat arbitrary private IP accress
 EthernetServer server(80); //port 80 is the default HTTP port
 const int sspin = 10; //D10 pin is the default Ethernet shield chip select pin
+const int sdpin = 9; //D9 pin is chip select for SD Card
 
 
-const float powerFactor = (4.56 / 1024.0)*(5.0/250); //for converting A/D reads into power
+const float powerFactor = (4.56 / 1024.0)*(250.0/5.0); //for converting A/D reads into power
 const float tempFactor = (4.56 / 1024) * 100.0; //for converting A/D reads to deg K
 const float ampThreshold = 66.0; //votage value for 66 degrees C temperature
 const int ampPin = 2; //amplifier on - off control TODO: determine the level
 const int doorPin = 3; //door open/close sensor LOW = open
+
+String ampStatus = "Off";
 
 
 void setup() {
@@ -35,17 +38,23 @@ void setup() {
   */
   
   //standard serial and Ethernet start up procedure
+  pinMode(sdpin, OUTPUT);
+  digitalWrite(sdpin, HIGH);
   Serial.begin(9600);
+
   Ethernet.init(sspin);
+  SPI.begin();
   Ethernet.begin(mac, ip);
 
   // Check for Ethernet hardware present
+  
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-  }
   if (Ethernet.linkStatus() == LinkOFF) {
     Serial.println("Ethernet cable is not connected.");
   }
+  }
+
   // start the server
   server.begin();
   Serial.print("server is at ");
@@ -53,12 +62,15 @@ void setup() {
 
   pinMode(ampPin, OUTPUT); //Amp enable
   pinMode(doorPin, INPUT);  //Door sensor
+ 
+  
+  
 }
 
 void loop() {
 
-  char reportText; //text to be sent to the host based on the type of request
-  float reportValue; //value, if any, to be sent to the host based on the type of request
+  String reportText; //text to be sent to the host based on the type of request
+  String reportValue; //value, if any, to be sent to the host based on the type of request
   
   int ampSensor = analogRead(A3);
   int sinkSensor = analogRead(A6);
@@ -68,59 +80,33 @@ void loop() {
   float sinkTemp = sinkSensor * tempFactor - 273.15;
   float airTemp = airSensor * tempFactor - 273.15;
 
-  Serial.println();
-  Serial.print("Amp Power (Watts): ");
-  Serial.println(ampPower);
-  Serial.print("Heatsink Temperature (deg C): ");
-  Serial.println(sinkTemp);
-  Serial.print("Air Temperature (deg C): ");
-  Serial.println(airTemp);
-
-  if (sinkTemp > ampThreshold) {
-    digitalWrite(ampPin, LOW);
-    Serial.print("Amp too hot.  It is: ");
-    Serial.print(sinkTemp);
-    Serial.println(" Deg C");
-  }
-
   int doorStatus = digitalRead(doorPin);
-  if (doorStatus == LOW) {
-    Serial.println("Door Open");
-  }
+  
 
   /*The head end is polled by the base station on a regular basis.  When polled by the base
   station (roughly every 2 seconds as of now), it provides the requested data to the base
   station.  It is envisioned that the head end querries all the data, one at a time, in each
   interval and displays them.  The base station can also command the head end to turn the
   amplifier on and off*/
-  
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    /*The queries and commands from the base have the form of ?q=x where ? signifies the start
-    of a query string, q signfiers that there is a query (or command) will follow the ?.  The 
-    queries and commands are:
-    a: provide air temperature
-    h: provide heatsink temperature
-    p: provide power level
-    t: turn on power amp
-    f: turn off power amp
-    */
-    /*request status keeps track of where the scanner is in the command sequence
+  /*request status keeps track of where the scanner is in the command sequence
     0: idle state
     1: ? detected
     2: q detected
     3: = detected - look for the command
     4: command is obtained
     */
-    int requestStatus = 0;
+   int requestStatus = 0;
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    //Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+        //Serial.print(c);
 
         //--------------------------- parsing the request ----------------------------------------
         //this comes first so when detecting = increments statusRequest to 3, the next charachter
@@ -128,48 +114,54 @@ void loop() {
         
         if (requestStatus == 3) {
           switch (c) {
-            case 'a': //requesting air temperature
-              reportText = "Air temperature is: ";
-              reportValue = airTemp;
-              requestStatus++;
-            case 'h': //requesting heatsink temperature
-              reportText = "Heatsink temperature is: ";
-              reportValue = sinkTemp;
-              requestStatus++;
-            case 'p': //requesting power level
-              reportText = "Power output is: ";
-              reportValue = ampPower;
-               requestStatus++;
-            case 'd': //requesting door status
-              reportText = "Door is closed";
-              reportValue = 0.0;
-              if (doorStatus == LOW) {
-                reportText = "Door is open";
+            case 'r': //report status
+              reportText = "airTemp=";
+              reportValue = String(airTemp, 1);
+              reportText += reportValue;
+              reportText += ";";
+              reportText+= "sinkTemp=";
+              reportValue = String(sinkTemp, 1);
+              reportText+= reportValue;
+              reportText+= ";";
+              reportText+= "ampPower=";
+              reportValue = String(ampPower, 1);
+              reportText+= reportValue;
+              reportText+= ";";
+              reportText+= "doorStatus=";
+              if (doorStatus == LOW){
+                reportText+= "Open";
+              } else {
+                reportText+= "Closed";
               }
+              reportText+= ";";
+              reportText+= "ampStatus=";
+              reportText+= ampStatus;
               requestStatus++;
+              break;
             case 't': //turn on power amp
               digitalWrite(ampPin, HIGH);
-              reportText = "Turned on power amp";
-              reportValue = 0.0;
+              ampStatus = "On";
+              reportText = "nothing=nothing";//to pacify the error handler on the other side
               requestStatus++;
+               break;
             case 'f':
               digitalWrite(ampPin, LOW);
-              reportText = "Turned Off power amp";
-              reportValue = 0.0;
+              ampStatus = "Off";
+              reportText = "nothing=nothing"; //to pacify the error handler on the other side
               requestStatus++;
+               break;
             default:
-              reportText = "Bad request";
-              reportValue = 0.0;
+              reportText = "badReq";
               requestStatus++;
           }
         }
         if (c == '?') {
           requestStatus = 1;
         }
-        if (c == "q" && requestStatus == 1) {
+        if (c == 'q' && requestStatus == 1) {
           requestStatus++;
         }
-        if (c == "=" && requestStatus == 2) {
+        if (c == '=' && requestStatus == 2) {
           requestStatus++;
         }
 
@@ -184,19 +176,13 @@ void loop() {
         if (c == '\n' && currentLineIsBlank) {
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
+          client.println("Content-Type: text/plain");
           client.println("Connection: close");  // the connection will be closed after completion of the response
           client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output reportText and reportValue
-          
-          client.print(reportText);
-          client.println(reportValue);
-          client.println("<br />");
-          
-          client.println("</html>");
+          client.println(reportText);
+          //break out of the loop when done
+          break;
         }
         if (c == '\n') {
           // you're starting a new line
@@ -211,8 +197,8 @@ void loop() {
     delay(1);
     // close the connection:
     client.stop();
-    Serial.println("client disconnected");
+    //Serial.println("client disconnected");
   }
   
-  delay(2000);
+  delay(100);
 }
